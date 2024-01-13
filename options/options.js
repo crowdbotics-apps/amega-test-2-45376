1634b8606d033b745563deea1c9b40dc3d2d4e28
Crowdbotics <team@crowdbotics.com>
2024-01-13 13:23:51 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "amega-test-2-45376",
+  url: "https://amega-test-2-45376.botics.co",
+  api: "https://amega-test-2-45376.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
