5fdc2d389f0e2184f25b4bd8aae64466c68be484
Evgeny Protopopov <e.protopopov@amegafx.com>
2024-01-13 15:27:34 +0200

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,5 +1,3 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "amega-test-2-45376",
   url: "https://amega-test-2-45376.botics.co",
@@ -7,9 +5,9 @@ export const globalOptions = {
   screenOptions: {
     headerShown: false
   }
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
